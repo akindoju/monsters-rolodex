8c826de3389c9ce81735ef79077fb596ddfa1c84
Akndju <davidd.akindoju@gmail.com>
2020-10-07 14:42:42 +0100

    Updates

---
+++ precache-manifest.2dbe360d99875c639984471f36582ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47ce6f1d8d0bde6536c189926eab5d4",
+    "url": "/akindoju/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad74cc273295842f308e",
+    "url": "/akindoju/github.io/monsters-rolodex/static/css/main.a97978f7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/akindoju/github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/akindoju/github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad74cc273295842f308e",
+    "url": "/akindoju/github.io/monsters-rolodex/static/js/main.824034ad.chunk.js"
+  },
+  {
+    "revision": "ad83f1d175943610f8b6",
+    "url": "/akindoju/github.io/monsters-rolodex/static/js/runtime-main.6f7bc92a.js"
+  }
+]);
